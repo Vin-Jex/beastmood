5e2c865fd3c058a098243b09a92b18fa13fe82f9
OKEREKE VINCENT <vincentcode0@gmail.com>
2023-05-10 17:14:24 +0100

    Feat: Create Species section

---
+++ client/components/Home/Specials/SpecialsSidenav.tsx
@@ -0,0 +1,39 @@
+import { ArrowForwardIos } from "@mui/icons-material";
+import React, { useState } from "react";
+
+export default function SpecialsSidenav() {
+  const [activeClass, setActiveClass] = useState(1);
+  return (
+    <ul className='specials-nav'>
+      <li
+        onClick={() => setActiveClass(1)}
+        className={` ${activeClass === 1 ? "specials-active" : ""}`}
+      >
+        <p>Recomended</p>
+        <ArrowForwardIos className='icon' />
+      </li>
+      <li
+        onClick={() => setActiveClass(2)}
+        className={` ${activeClass === 2 ? "specials-active" : ""}`}
+      >
+        <p>Favorite&apos;s</p>
+        <ArrowForwardIos className='icon' />
+      </li>
+
+      <li
+        onClick={() => setActiveClass(3)}
+        className={` ${activeClass === 3 ? "specials-active" : ""}`}
+      >
+        <p>Completed</p>
+        <ArrowForwardIos className='icon' />
+      </li>
+      <li
+        onClick={() => setActiveClass(4)}
+        className={` ${activeClass === 4 ? "specials-active" : ""}`}
+      >
+        <p>Coming Up</p>
+        <ArrowForwardIos className='icon' />
+      </li>
+    </ul>
+  );
+}
