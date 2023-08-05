8629ee1862206267d9a556143a65610855d91831
OKEREKE VINCENT <vincentcode0@gmail.com>
2023-08-05 22:32:30 +0100

    Fix: Delete Server Files

--- server/crypto.js
+++
@@ -1,9 +0,0 @@
-const crypto = require("crypto");
-
-//32 bytes key (256 bits)
-const generateRandomSecretKey = () => {
-  return crypto.randomBytes(32).toString("hex");
-};
-
-const secretKey = generateRandomSecretKey();
-console.log("Random Secret Key:", secretKey);
