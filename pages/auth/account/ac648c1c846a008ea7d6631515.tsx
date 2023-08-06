9520572a8649c693855fd3aac10ffb55ed164d6e
OKEREKE VINCENT <vincentcode0@gmail.com>
2023-08-06 07:05:42 +0100

    Feat: Create Signup form 2

---
+++ pages/auth/account/ac648c1c846a008ea7d6631515.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout/Layout";
+import React from "react";
+
+const ac648c1c846a008ea7d6631515 = () => {
+  return (
+    <Layout>
+      <div>ac648c1c846a008ea7d6631515</div>
+    </Layout>
+  );
+};
+
+export default ac648c1c846a008ea7d6631515;
