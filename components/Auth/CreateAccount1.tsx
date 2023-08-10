32e64b91d1a572acc5f01fbba6ea5daa456c2066
OKEREKE VINCENT <vincentcode0@gmail.com>
2023-08-10 13:51:07 +0100

    Feat: Add Auth Pages

--- components/Auth/CreateAccount1.tsx
+++ components/Auth/CreateAccount1.tsx
@@ -5,28 +5,63 @@ import Link from "next/link";
 import Button from "../Molecules/Input/Button";
 import AuthBG from "@/public/images/authbg.png";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
-export default function Login() {
+export default function CreateAccount1() {
+  const route = useRouter()
   return (
     <div className='flex justify-center items-center h-screen overflow-hidden'>
       <Image
         src={AuthBG}
         alt='Auth Background'
-        className='mt-[4rem] -mr-[8rem] -z-10'
+        className='mt-[6rem] -mr-[8rem]  -z-10'
       />
 
       <div className='w-full sm:w-[600px] flex flex-col items-center justify-center p-10 my-auto'>
         <div className='flex flex-col justify-center items-center mb-4'>
           <span className='xl:text-3xl lg:text-2xl md:text-xl text-lg text-main-brand leading-4 font-bold'>
-            Log into your account
+            Create an account
           </span>
-          <p className='text-[#020202]/70 dark:text-light/70 text-sm'>
-            Log into your BeastMood Account
+          <p className='text-[#020202]/70 dark:text-light/70 text-left sm:text-center text-sm w-[90%]'>
+            Create your beastmood account to like, add to fave and leave
+            comments!
           </p>
         </div>
 
-        <form className='w-full px-12 mt-4 space-y-3'>
-          <div className='space-y-3'>
+        <form className='w-full px-12 mt-4 space-y-4'>
+          <div className='space-y-2'>
+            <div className='space-y-1'>
+              <label
+                htmlFor='firstName'
+                className='text-dark dark:text-light text-base'
+              >
+                First Name
+              </label>
+              <Input
+                id='firstName'
+                type='text'
+                name='firstName'
+                className='input'
+                placeholder='Enter your first name'
+              />
+            </div>
+
+            <div className='space-y-1'>
+              <label
+                htmlFor='lastName'
+                className='text-dark dark:text-light text-base'
+              >
+                Last Name
+              </label>
+              <Input
+                id='lastName'
+                type='text'
+                name='lastName'
+                className='input'
+                placeholder='Enter your last name'
+              />
+            </div>
+
             <div className='space-y-1'>
               <label
                 htmlFor='email'
@@ -37,12 +72,12 @@ export default function Login() {
               <Input
                 id='email'
                 type='email'
+                name='email'
                 className='input'
-                // showIcon={Visibility}
-                // hideIcon={HideImageRounded}
                 placeholder='Enter your email'
               />
             </div>
+
             <div className='space-y-1'>
               <label
                 htmlFor='email'
@@ -56,53 +91,39 @@ export default function Login() {
                 className='input'
                 showIcon={VisibilityOutlined}
                 hideIcon={VisibilityOffOutlined}
-                placeholder='•••••••••••'
+                placeholder='Enter Password'
               />
-            </div>
-            <div className='flex justify-between items-center !my-4'>
-              <div className='customCheckbox'>
-                <input
-                  type='checkbox'
-                  id='rememberMe'
-                  className='bg-transparent border-dark dark:border-light'
-                />
-                <label
-                  htmlFor='rememberMe'
-                  className='text-gray-800 dark:text-light text-sm'
-                >
-                  Remember me
-                </label>
-              </div>
-              <span className='text-main-brand text-sm'>
-                <Link href='/auth/forget-password'>Forget Password?</Link>
-              </span>
             </div>
             <Button
               type='submit'
-              className='btn bg-main-brand disabled:bg-dark3 disabled:text-light4 text-white text-[.9rem] py-3 hover:scale-[.98] transition-all ease-in-out duration-300'
+              onClick={(e) => {
+                e.preventDefault();
+                route.push("/auth/account/ac648c1c846a008ea7d6631515");
+              }}
+              className='btn bg-main-brand disabled:bg-dark3 disabled:text-light4 text-white text-[.9rem] py-3.5 !mt-4 hover:scale-[.98] transition-all ease-in-out duration-300'
             >
-              Login
+              Continue
             </Button>
           </div>
 
-          <div className='w-full pt-2'>
+          <div className='w-full pt-1'>
             <Button
               type='button'
-              className='btn bg-transparent border-[2px] border-[#B5AFFF] disabled:bg-dark3 disabled:text-dark2 text-dark dark:text-light/70 text-[.9rem] py-3 hover:scale-[.98] transition-all ease-in-out duration-300'
+              className='btn bg-transparent border-[2px] border-[#B5AFFF] disabled:bg-dark3 disabled:text-dark2 text-dark dark:text-light/70 text-[.9rem] py-3.5 hover:scale-[.98] transition-all ease-in-out duration-300'
             >
-              Log in with Google
+              Sign up with Google
             </Button>
           </div>
 
           <div className='w-full flex items-center justify-center gap-1 text-lg'>
             <span className='text-gray-800 dark:text-light/90 text-sm'>
-              Don&apos;t have an account?{" "}
+              Already have an account?{" "}
             </span>
             <Link
-              href='/auth/account/ac648c1c846a008ea7d6631505'
+              href='/auth/login'
               className='text-main-brand font-semibold text-sm'
             >
-              Sign Up
+              Login
             </Link>
           </div>
         </form>
---
+++ components/Auth/CreateAccount2.tsx
@@ -0,0 +1,121 @@
+import React, { useRef, useState } from "react";
+import Layout from "../Layout/Layout";
+import Input from "../Molecules/Input/Input";
+import Button from "../Molecules/Input/Button";
+import Link from "next/link";
+import Image, { StaticImageData } from "next/image";
+import bg from "@/public/images/Bungostraydogs-small-1.png";
+import { useRouter } from "next/router";
+import AvaterEditingModal from "../Molecules/Modal/AvaterEditingModal";
+
+const CreateAccount2 = () => {
+  const route = useRouter();
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [file, setFile] = useState<string | StaticImageData>(bg);
+  const [photoURL, setPhotoURL] = useState<string | StaticImageData>("");
+
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleButtonClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files?.[0];
+    if (selectedFile) {
+      const imageUrl = URL.createObjectURL(selectedFile);
+      setFile(imageUrl);
+      setModalOpen(true);
+    }
+  };
+
+  const handleModal = () => {
+    setModalOpen(!modalOpen);
+  };
+
+  console.log(file, "FDGBTRD");
+
+  return (
+    <div className='flex justify-center items-center h-screen overflow-hidden'>
+      <div className='w-full sm:w-[600px] flex flex-col items-center justify-center px-6 py-6 sm:p-8 md:p-10 my-auto'>
+        <div className='flex flex-col justify-center items-center mb-4'>
+          <span className='xl:text-3xl lg:text-2xl md:text-xl text-lg text-center w-[80%] text-main-brand leading-6 sm:leading-6 font-bold'>
+            Choose your Avatar and Username
+          </span>
+        </div>
+
+        <form className='w-full px-0 sm:px-4 md:px-12 mt-4 space-y-4'>
+          <div className='w-full flex flex-col items-center justify-center gap-y-8 !mb-8'>
+            <Image
+              src={!photoURL ? file : photoURL}
+              alt='avater'
+              width={100}
+              height={100}
+              className='w-20 h-20 md:w-28 md:h-28 rounded-full object-cover'
+            />
+
+            <div className="!mt-3">
+              <Button
+                type='button'
+                className='w-fit bg-transparent ring-2 ring-dark/80 dark:ring-light/80 text-dark dark:text-light text-sm px-4 py-3 capitalize'
+                onClick={handleButtonClick}
+              >
+                choose avatar
+              </Button>
+              <input
+                ref={fileInputRef}
+                type='file'
+                accept='image/png, image/jpeg, image/jpg, image/svg+xml'
+                style={{ display: "none" }}
+                onChange={handleFileChange}
+              />
+            </div>
+          </div>
+          <div className='space-y-2'>
+            <div className='space-y-1'>
+              <label
+                htmlFor='firstName'
+                className='text-dark dark:text-light text-sm sm:text-base'
+              >
+                Username
+              </label>
+              <Input
+                id='username'
+                type='text'
+                name='username'
+                className='input'
+                placeholder='Enter Username'
+              />
+              <span className='text-[.69rem] sm:text-xs text-dark/90 dark:text-light/90 px-1'>
+                Alphanumeric characters, hyphens and underscores only.
+              </span>
+            </div>
+
+            <Button
+              type='submit'
+              onClick={(e) => {
+                e.preventDefault();
+                route.push("/");
+              }}
+              className='btn mx-auto bg-main-brand disabled:bg-dark3 disabled:text-light4 text-white text-[.79rem] sm:text-[.9rem] px-8 py-3.5 !mt-12 hover:scale-[.98] transition-all ease-in-out duration-300 uppercase'
+            >
+              Continue to beastmood
+            </Button>
+          </div>
+        </form>
+      </div>
+
+      <AvaterEditingModal
+        setFileURL={setFile}
+        image={file as string}
+        handleModal={handleModal}
+        isModalOpen={modalOpen}
+        setPhotoURL={setPhotoURL}
+      />
+    </div>
+  );
+};
+
+export default CreateAccount2;
