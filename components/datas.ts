69ff1b8c97975515f87844bd172c0b9ba682f3b6
OKEREKE VINCENT <vincentcode0@gmail.com>
2023-08-10 14:00:05 +0100

    Feat: Create file for dummy datas's

--- components/datas.ts
+++ components/datas.ts
@@ -8,90 +8,116 @@ import {
 } from "@mui/icons-material";
 
 export const browserItems: {
-  genres: { text: string }[];
-  genres2: { text: string }[];
+  genres: { text: string, link: string}[];
+  genres2: { text: string, link: string}[];
 } = {
   genres: [
     {
       text: "actions",
+      link: "/"
     },
     {
       text: "adventure",
+      link: "/"
     },
     {
       text: "cars",
+      link: "/"
     },
     {
       text: "comedy",
+      link: "/"
     },
     {
       text: "dementia",
+      link: "/"
     },
     {
       text: "demons",
+      link: "/"
     },
     {
       text: "drama",
+      link: "/"
     },
     {
       text: "ecchi",
+      link: "/"
     },
     {
       text: "fantasy",
+      link: "/"
     },
     {
       text: "game",
+      link: "/"
     },
     {
       text: "harem",
+      link: "/"
     },
     {
       text: "historical",
+      link: "/"
     },
 
     {
       text: "horror",
+      link: "/"
     },
     {
       text: "isekai",
+      link: "/"
     },
   ],
   genres2: [
     {
       text: "josei",
+      link: "/"
     },
     {
       text: "kids",
+      link: "/"
     },
     {
       text: "magic",
+      link: "/"
     },
     {
       text: "martial arts",
+      link: "/"
     },
     {
       text: "mecha",
+      link: "/"
     },
     {
       text: "military",
+      link: "/"
     },
     {
       text: "music",
+      link: "/"
     },
     {
       text: "mystery",
+      link: "/"
     },
     {
       text: "perody",
+      link: "/"
     },
     {
       text: "police",
+      link: "/"
     },
     {
       text: "christian",
+      link: "/"
     },
     {
       text: "toon",
+      link: "/"
     },
   ],
 };
