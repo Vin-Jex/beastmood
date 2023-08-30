8919356117327be97f795bc120e4968294f9ed63
OKEREKE VINCENT <vincentcode0@gmail.com>
2023-08-30 02:01:40 +0100

    Feat: Update rating component

--- components/Home/Rating.tsx
+++ components/Home/Rating.tsx
@@ -3,17 +3,7 @@ import React from "react";
 import Img from "@/public/images/small.png";
 import Img2 from "@/public/images/Rectangle 24.png";
 import { ArrowForward } from "@mui/icons-material";
-
-const originalArray = [0, 0, 0, 0, 0];
-let copyArray = [...originalArray]; // a copy of the original array
-
-const userInput = 5;
-
-console.log("Number: ", originalArray[1]);
-
-copyArray[userInput - 1] = copyArray[userInput - 1] + 1;
-
-console.log(copyArray, originalArray);
+import RatingCard from "../Molecules/ratingCard";
 
 const Rating = () => {
   return (
@@ -21,10 +11,10 @@ const Rating = () => {
       <div className='flex flex-col w-full col-span-1 px-4 justify-center items-center'>
         <div className='w-full flex flex-col justify-center'>
           <Image src={Img} alt='Avater' className='w-[12em] !mb-3 mx-auto' />
-          <span className='text-dark dark:text-light text-lg md:text-xl xl:text-2xl text-center font-medium capitalize'>
+          <span className='text-dark dark:text-light text-base md:text-lg xl:text-xl text-center font-medium capitalize'>
             Top Rated Manga
           </span>
-          <p className='text-dark dark:text-light text-base md:text-[1.1rem] text-center'>
+          <p className='text-dark dark:text-light text-sm md:text-[.9rem] text-center'>
             Checkout all completed series with best ratings.
           </p>
         </div>
@@ -37,209 +27,79 @@ const Rating = () => {
 
       <div className='col-span-2 lg:col-span-4 w-full flex flex-row flex-wrap sm:flex-nowrap gap-y-20 sm:gap-y-0 justify-evenly items-center ml-0 lg:ml-32'>
         <div className='px-4 py-2 flex flex-col w-full'>
-          <span className='text-dark dark:text-light text-xl capitalize mb-8'>
+          <span className='text-dark dark:text-light text-lg capitalize mb-8'>
             Weekly ranking
           </span>
 
-          <ul className='flex flex-col space-y-10'>
-            <li className='flex gap-6 lg:gap-8'>
-              <span className='text-dark dark:text-light xl:text-xl font-medium'>
-                1.
-              </span>
-
-              <Image
-                src={Img2}
-                alt='Manga one'
-                className='w-[4rem] xl:w-[5rem] h-[4rem] xl:h-[5rem] rounded-lg object-cover'
-              />
-              <div className='flex flex-col space-y-2'>
-                <span className='text-dark dark:text-light text-lg lg:text-xl font-medium whitespace-nowrap'>
-                  Shingeki no Kyogin...
-                </span>
-                <p className='text-dark dark:text-light text-base xl:text-lg'>
-                  紗子 Thriller
-                </p>
-              </div>
-            </li>
-            <li className='flex gap-6 lg:gap-8'>
-              <span className='text-dark dark:text-light xl:text-xl font-medium'>
-                2.
-              </span>
-
-              <Image
-                src={Img2}
-                alt='Manga one'
-                className='w-[4rem] xl:w-[5rem] h-[4rem] xl:h-[5rem] rounded-lg object-cover'
-              />
-              <div className='flex flex-col space-y-2'>
-                <span className='text-dark dark:text-light text-lg lg:text-xl font-medium whitespace-nowrap'>
-                  Shingeki no Kyogin...
-                </span>
-                <p className='text-dark dark:text-light text-base xl:text-lg'>
-                  紗子 Thriller
-                </p>
-              </div>
-            </li>
-            <li className='flex gap-6 lg:gap-8'>
-              <span className='text-dark dark:text-light xl:text-xl font-medium'>
-                3.
-              </span>
-
-              <Image
-                src={Img2}
-                alt='Manga one'
-                className='w-[4rem] xl:w-[5rem] h-[4rem] xl:h-[5rem] rounded-lg object-cover'
-              />
-              <div className='flex flex-col space-y-2'>
-                <span className='text-dark dark:text-light text-lg lg:text-xl font-medium whitespace-nowrap'>
-                  Shingeki no Kyogin...
-                </span>
-                <p className='text-dark dark:text-light text-base xl:text-lg'>
-                  紗子 Thriller
-                </p>
-              </div>
-            </li>
-            <li className='flex gap-6 lg:gap-8'>
-              <span className='text-dark dark:text-light xl:text-xl font-medium'>
-                4.
-              </span>
-
-              <Image
-                src={Img2}
-                alt='Manga one'
-                className='w-[4rem] xl:w-[5rem] h-[4rem] xl:h-[5rem] rounded-lg object-cover'
-              />
-              <div className='flex flex-col space-y-2'>
-                <span className='text-dark dark:text-light text-lg lg:text-xl font-medium whitespace-nowrap'>
-                  Shingeki no Kyogin...
-                </span>
-                <p className='text-dark dark:text-light text-base xl:text-lg'>
-                  紗子 Thriller
-                </p>
-              </div>
-            </li>
-            <li className='flex gap-6 lg:gap-8'>
-              <span className='text-dark dark:text-light xl:text-xl font-medium'>
-                5.
-              </span>
-
-              <Image
-                src={Img2}
-                alt='Manga one'
-                className='w-[4rem] xl:w-[5rem] h-[4rem] xl:h-[5rem] rounded-lg object-cover'
-              />
-              <div className='flex flex-col space-y-2'>
-                <span className='text-dark dark:text-light text-lg lg:text-xl font-medium whitespace-nowrap'>
-                  Shingeki no Kyogin...
-                </span>
-                <p className='text-dark dark:text-light text-base xl:text-lg'>
-                  紗子 Thriller
-                </p>
-              </div>
-            </li>
+          <ul className='flex flex-col space-y-6'>
+            <RatingCard
+              index={1}
+              title='Shingeki no Kyogin...'
+              subTitle=' 紗子 Thriller'
+              imageSrc={Img2}
+            />
+            <RatingCard
+              index={2}
+              title='Shingeki no Kyogin...'
+              subTitle=' 紗子 Thriller'
+              imageSrc={Img2}
+            />
+            <RatingCard
+              index={3}
+              title='Shingeki no Kyogin...'
+              subTitle=' 紗子 Thriller'
+              imageSrc={Img2}
+            />
+            <RatingCard
+              index={4}
+              title='Shingeki no Kyogin...'
+              subTitle=' 紗子 Thriller'
+              imageSrc={Img2}
+            />
+            <RatingCard
+              index={5}
+              title='Shingeki no Kyogin...'
+              subTitle=' 紗子 Thriller'
+              imageSrc={Img2}
+            />
           </ul>
         </div>
         <div className='px-4 py-2 flex flex-col w-full'>
-          <span className='text-dark dark:text-light text-xl capitalize mb-8'>
-            Weekly ranking
+          <span className='text-dark dark:text-light text-lg capitalize mb-8'>
+            Top Genres
           </span>
 
-          <ul className='flex flex-col space-y-10'>
-            <li className='flex gap-6 lg:gap-8'>
-              <span className='text-dark dark:text-light xl:text-xl font-medium'>
-                1.
-              </span>
-
-              <Image
-                src={Img2}
-                alt='Manga one'
-                className='w-[4rem] xl:w-[5rem] h-[4rem] xl:h-[5rem] rounded-lg object-cover'
-              />
-              <div className='flex flex-col space-y-2'>
-                <span className='text-dark dark:text-light text-lg lg:text-xl font-medium whitespace-nowrap'>
-                  Shingeki no Kyogin...
-                </span>
-                <p className='text-dark dark:text-light text-base xl:text-lg'>
-                  紗子 Thriller
-                </p>
-              </div>
-            </li>
-            <li className='flex gap-6 lg:gap-8'>
-              <span className='text-dark dark:text-light xl:text-xl font-medium'>
-                2.
-              </span>
-
-              <Image
-                src={Img2}
-                alt='Manga one'
-                className='w-[4rem] xl:w-[5rem] h-[4rem] xl:h-[5rem] rounded-lg object-cover'
-              />
-              <div className='flex flex-col space-y-2'>
-                <span className='text-dark dark:text-light text-lg lg:text-xl font-medium whitespace-nowrap'>
-                  Shingeki no Kyogin...
-                </span>
-                <p className='text-dark dark:text-light text-base xl:text-lg'>
-                  紗子 Thriller
-                </p>
-              </div>
-            </li>
-            <li className='flex gap-6 lg:gap-8'>
-              <span className='text-dark dark:text-light xl:text-xl font-medium'>
-                3.
-              </span>
-
-              <Image
-                src={Img2}
-                alt='Manga one'
-                className='w-[4rem] xl:w-[5rem] h-[4rem] xl:h-[5rem] rounded-lg object-cover'
-              />
-              <div className='flex flex-col space-y-2'>
-                <span className='text-dark dark:text-light text-lg lg:text-xl font-medium whitespace-nowrap'>
-                  Shingeki no Kyogin...
-                </span>
-                <p className='text-dark dark:text-light text-base xl:text-lg'>
-                  紗子 Thriller
-                </p>
-              </div>
-            </li>
-            <li className='flex gap-6 lg:gap-8'>
-              <span className='text-dark dark:text-light xl:text-xl font-medium'>
-                4.
-              </span>
-
-              <Image
-                src={Img2}
-                alt='Manga one'
-                className='w-[4rem] xl:w-[5rem] h-[4rem] xl:h-[5rem] rounded-lg object-cover'
-              />
-              <div className='flex flex-col space-y-2'>
-                <span className='text-dark dark:text-light text-lg lg:text-xl font-medium whitespace-nowrap'>
-                  Shingeki no Kyogin...
-                </span>
-                <p className='text-dark dark:text-light text-base xl:text-lg'>
-                  紗子 Thriller
-                </p>
-              </div>
-            </li>
-            <li className='flex gap-6 lg:gap-8'>
-              <span className='text-dark dark:text-light xl:text-xl font-medium'>
-                5.
-              </span>
-
-              <Image
-                src={Img2}
-                alt='Manga one'
-                className='w-[4rem] xl:w-[5rem] h-[4rem] xl:h-[5rem] rounded-lg object-cover'
-              />
-              <div className='flex flex-col space-y-2'>
-                <span className='text-dark dark:text-light text-lg lg:text-xl font-medium whitespace-nowrap'>
-                  Shingeki no Kyogin...
-                </span>
-                <p className='text-dark dark:text-light text-base xl:text-lg'>
-                  紗子 Thriller
-                </p>
-              </div>
-            </li>
+          <ul className='flex flex-col space-y-6'>
+            <RatingCard
+              index={1}
+              title='Shingeki no Kyogin...'
+              subTitle=' 紗子 Thriller'
+              imageSrc={Img2}
+            />
+            <RatingCard
+              index={2}
+              title='Shingeki no Kyogin...'
+              subTitle=' 紗子 Thriller'
+              imageSrc={Img2}
+            />
+            <RatingCard
+              index={3}
+              title='Shingeki no Kyogin...'
+              subTitle=' 紗子 Thriller'
+              imageSrc={Img2}
+            />
+            <RatingCard
+              index={4}
+              title='Shingeki no Kyogin...'
+              subTitle=' 紗子 Thriller'
+              imageSrc={Img2}
+            />
+            <RatingCard
+              index={5}
+              title='Shingeki no Kyogin...'
+              subTitle=' 紗子 Thriller'
+              imageSrc={Img2}
+            />
           </ul>
         </div>
       </div>
