3bdf9e05ab769b1cd88359caecc7eb352e26f048
OKEREKE VINCENT <vincentcode0@gmail.com>
2023-11-09 23:02:04 +0100

    Fix: Bugfix

--- pages/auth/verification.tsx
+++ pages/auth/verification.tsx
@@ -1,5 +1,4 @@
 import Layout from "@/components/Layout/Layout";
-import Cookies from "js-cookie";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
@@ -7,39 +6,46 @@ const Verification = () => {
   const [dots, setDots] = useState(".");
   const [message, setMessage] = useState(`Verifying User ${dots}`);
   const router = useRouter();
-
-  const verify = async () => {
-    const { email, token } = router.query;
-    try {
-      const response = await fetch(
-        `https://api.beastmoodsee.com/api/v1/verify?email=${email}&token=${token}`
-      );
-
-      if (!response.ok) {
-        const responseData = await response.json();
-
-        if (
-          responseData.message ===
-            "Email and token are required to verify the email address!" ||
-          responseData.message ===
-            "Invalid verification link. Request another email verification link!"
-        ) {
-          setMessage("Invalid verification link.");
-        }
-      } else {
-        const responseData = await response.json();
-        setMessage(responseData?.message || "Email verification completed.");
-      }
-    } catch (error) {
-      setMessage((error as string) || "Email verification failed");
-      console.log("ERROR: " + error);
-      console.log("Verification email failed");
-    }
-  };
-
+  
+  const { email, token } = router.query;
+  
   useEffect(() => {
+    // console.log("Query: " + JSON.stringify(router.query));
+    const verify = async () => {
+      if (email && token) {
+        try {
+          const response = await fetch(
+            `https://api.beastmoodsee.com/api/v1/verify?email=${email}&token=${token}`
+          );
+
+          if (!response.ok) {
+            const responseData = await response.json();
+
+            if (
+              responseData.message ===
+                "Email and token are required to verify the email address!" ||
+              responseData.message ===
+                "Invalid verification link. Request another email verification link!"
+            ) {
+              setMessage("Invalid verification link.");
+            }
+          } else {
+            const responseData = await response.json();
+            setMessage(
+              responseData?.message || "Email verification completed."
+            );
+          }
+        } catch (error) {
+          setMessage((error as string) || "Email verification failed");
+
+          console.log("ERROR: " + error);
+          console.log("Verification email failed");
+        }
+      }
+    };
+
     verify();
-  }, []);
+  }, [email, router.query, token]);
 
   useEffect(() => {
     const interval = setInterval(() => {
